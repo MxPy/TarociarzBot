c2ddd1b5101382db47ef951a2b1aa53e0f4ddb73
Maximilian Patalas <maksymilianpatalas@gmail.com>
2023-11-24 16:16:55 +0100

    add image support

--- bot.py
+++ bot.py
@@ -1,13 +1,23 @@
 import discord
 import responses
 import os
+import tarot
 from dotenv import load_dotenv
 
 
 async def send_message(message, id, user_message, channel, is_private):
+    card = tarot.get_cards()
     try:
-        response = responses.handle_respons(id, user_message, channel)
-        await message.author.send(response) if is_private else await message.channel.send(response)
+        response = responses.handle_respons(id, user_message, card)
+        file_path = f"cards/{card['title']}.jpg"
+        
+        if is_private:
+            await message.author.send(response, file=discord.File(file_path))
+        else:
+            await message.channel.send(response, file=discord.File(file_path))
+
+    
+
     except Exception as e:
         print(e)
         
@@ -39,6 +49,7 @@ def run_discord_bot():
             if user_message[0] == '?':
                 user_message = user_message[1:]
                 await send_message(message, user_id, user_message, channel, is_private=True)
+                #TODO move this logic to responses
             elif user_message == 'wylosuj wszystkim':
                 memebers = [member for member in client.get_all_members() if not member.bot]
                 for user in memebers:
--- responses.py
+++ responses.py
@@ -1,6 +1,4 @@
-import tarot
-
-def handle_respons(id, message, channel) -> str:
+def handle_respons(id, message, card) -> str:
     p_message = message.lower()
     
     if p_message == "hello":
@@ -10,6 +8,5 @@ def handle_respons(id, message, channel) -> str:
         return "This is help message for tarociarz bot"
     
     if p_message =="wylosuj mi":
-        card = tarot.get_cards()
-        return f"<@{id}> Karta dla ciebie to **{card['title']}**\n{card['description']}"
+        return f"<@{id}> Karta dla ciebie to **{card['title']}**\n**{card['description']}**"
     
